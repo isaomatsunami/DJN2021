285dfe54b8ac2b30242bbd851ae8d1e5e514d430
isao matsunami <isao.matsunami@gmail.com>
2021-01-27 03:30:07 +0900

    corrected

--- scripts/collectFiles.ps1
+++ scripts/collectFiles.ps1
@@ -2,6 +2,7 @@
 # そのコピーをcollectedDirectoryに集める
 # $targetDirectoryの中に$collectedDirectoryがあると、コピーしたものが再びコピーの対象になるので
 # エラーが出るかもしれない。コピー先のディレクトリは対象ディレクトリの外に置くべき
+# 別のフォルダに同じ名前のファイルがある場合、上書きされてしまう（避けたい場合は名前を変更すべきかもしれない）
 
 $targetDirectory = "scripts"
 $collectedDirectory = "collectedFiles"
